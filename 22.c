7d7f526dbe2813b6c3b6554ab136988a29768811
Teraiyaabhishek <165642406+Teraiyaabhishek@users.noreply.github.com>
2024-04-14 13:26:19 +0530

    22

---
+++ 22.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 100
+
+int queue[MAX_SIZE];
+int front = -1, rear = -1;
+
+int isEmpty() {
+    return (front == -1 && rear == -1);
+}
+
+int isFull() {
+    return (rear == MAX_SIZE - 1);
+}
+
+void enqueue(int value) {
+    if (isFull()) {
+        printf("Queue is full\n");
+        return;
+    } else if (isEmpty()) {
+        front = rear = 0;
+    } else {
+        rear++;
+    }
+    queue[rear] = value;
+}
+
+int dequeue() {
+    int deletedElement;
+    if (isEmpty()) {
+        printf("Queue is empty\n");
+        exit(1);
+    } else if (front == rear) {
+        deletedElement = queue[front];
+        front = rear = -1;
+    } else {
+        deletedElement = queue[front];
+        front++;
+    }
+    return deletedElement;
+}
+
+void displayQueue() {
+    if (isEmpty()) {
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("Queue elements: ");
+    for (int i = front; i <= rear; i++) {
+        printf("%d ", queue[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    
+    enqueue(10);
+    enqueue(20);
+    enqueue(30)
+    printf("Before deletion: ");
+    displayQueue();
+
+
+    int deletedElement = dequeue();
+    printf("Deleted element: %d\n", deletedElement);
+
+    printf("After deletion: ");
+    displayQueue();
+
+    return 0;
+}
