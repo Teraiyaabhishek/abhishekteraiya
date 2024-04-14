19820bcf215905853c081565788b8a325d7b9fa6
Teraiyaabhishek <165642406+Teraiyaabhishek@users.noreply.github.com>
2024-04-14 13:25:39 +0530

    19

---
+++ 19.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int stack[MAX_SIZE];
+int top = -1;
+
+int isFull() {
+    return top == MAX_SIZE - 1;
+}
+void push(int value) {
+    if (isFull()) {
+        printf("Stack overflow!\n");
+        return;
+    }
+    stack[++top] = value;
+    printf("Element %d \n", value);
+}
+void display() {
+    if (top == -1) {
+        printf("Stack is empty. No elements to display.\n");
+        return;
+    }
+    printf("Elements in the stack: ");
+    for (int i = 0; i <= top; i++) {
+        printf("%d ", stack[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    display();
+
+    return 0;
+}
